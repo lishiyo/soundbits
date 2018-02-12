3965463a79ea2810f86dcc26ff3a350b6ead2217
connieli <c.ziye.li@gmail.com>
2018-02-11 23:50:28 -0500

    add fragment title, fix tempo normalization

--- app/src/main/java/com/cziyeli/songbits/playlistcard/TrackStatsView.kt
+++ app/src/main/java/com/cziyeli/songbits/playlistcard/TrackStatsView.kt
@@ -2,8 +2,10 @@ package com.cziyeli.songbits.playlistcard
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.View
 import android.widget.LinearLayout
 import com.cziyeli.commons.Utils
 import com.cziyeli.domain.summary.TrackListStats
@@ -16,7 +18,7 @@ class TrackStatsView@JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    data class StatLabel(val title: String, val range: Pair<Int, Int>)
+    data class StatLabel(val title: String, val titleNumber: String, val range: Pair<Int, Int>)
 
     companion object {
         // range is 0 - 8
@@ -47,6 +49,18 @@ class TrackStatsView@JvmOverloads constructor(
             title_row_one.height = FULL_HEIGHT_PX
             title_row_two.height = FULL_HEIGHT_PX
             title_row_three.height = FULL_HEIGHT_PX
+
+            val numberSize = resources.getDimension(R.dimen.default_title_size)
+            title_row_one_number.setTextSize(TypedValue.COMPLEX_UNIT_PX, numberSize)
+            title_row_two_number.setTextSize(TypedValue.COMPLEX_UNIT_PX, numberSize)
+            title_row_three_number.setTextSize(TypedValue.COMPLEX_UNIT_PX, numberSize)
+
+            title_row_one.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+            title_row_two.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+            title_row_two.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+            title_row_one_number.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+            title_row_two_number.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+            title_row_three_number.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
         }
     }
 
@@ -63,30 +77,36 @@ class TrackStatsView@JvmOverloads constructor(
             loadAudioFeatures(
                     StatLabel(
                             getStatTitle("danceable", normalizedDanceability, trackStats.avgDanceability),
+                            getStatTitleNum(trackStats.avgDanceability, 1.0),
                             Pair(0, normalizedDanceability.roundToInt())
                     ),
                     StatLabel(
                             getStatTitle("energetic", normalizedEnergy, trackStats.avgEnergy),
+                            getStatTitleNum(trackStats.avgEnergy, 1.0),
                             Pair(0, normalizedEnergy.roundToInt())),
                     StatLabel(
                             getStatTitle("positive", normalizedValence, trackStats.avgValence),
+                            getStatTitleNum(trackStats.avgValence, 1.0),
                             Pair(0, normalizedValence.roundToInt())
                     ))
         } else {
             val normalizedPopularity = getNormalizedStat(trackStats.avgPopularity!! / 100)
             val normalizedAcousticness = getNormalizedStat(trackStats.avgAcousticness)
-            val normalizedTempo = getNormalizedStat(trackStats.avgTempo / (200-40))
+            val normalizedTempo = getNormalizedStat((trackStats.avgTempo-60) / (200-60))
 
             loadAudioFeatures(
                     StatLabel(
                             getStatTitle("popular", normalizedPopularity, trackStats.avgPopularity!!),
+                            getStatTitleNum(trackStats.avgPopularity!!, 100.0),
                             Pair(0, normalizedPopularity.roundToInt())
                     ),
                     StatLabel(
                             getStatTitle("acoustic", normalizedAcousticness, trackStats.avgAcousticness),
+                            getStatTitleNum(trackStats.avgAcousticness, 1.0),
                             Pair(0, normalizedAcousticness.roundToInt())),
                     StatLabel(
                             getStatTitle("high-tempo", normalizedTempo, trackStats.avgTempo),
+                            getStatTitleNum(trackStats.avgTempo, 200.0),
                             Pair(0, normalizedTempo.roundToInt())
                     ))
         }
@@ -96,32 +116,35 @@ class TrackStatsView@JvmOverloads constructor(
         return value * TRACK_RANGE
     }
 
+    private fun getStatTitleNum(originalVal: Double, totalVal: Double) : String {
+        return "${"%.2f".format(originalVal)} / ${"%.1f".format(totalVal)}"
+    }
+
     private fun getStatTitle(stat: String, normalizedValue: Double, originalVal: Double) : String {
         // not at all (0-2), less (2-4), more (4-6), very (6-8)
         return when {
-            !twoColumnView && normalizedValue < QUARTILE -> "not at all $stat ~ ${"%.2f".format(originalVal)}"
-            !twoColumnView && normalizedValue < QUARTILE * 2 -> "less $stat ~ ${"%.2f".format(originalVal)}"
-            !twoColumnView && normalizedValue < QUARTILE * 3 -> "more $stat ~ ${"%.2f".format(originalVal)}"
-            !twoColumnView -> "super $stat ~ ${"%.2f".format(originalVal)}"
-            twoColumnView && normalizedValue < QUARTILE -> "not at all $stat \n${"%.2f".format(originalVal)}"
-            twoColumnView && normalizedValue < QUARTILE * 2 -> "less $stat \n${"%.2f".format(originalVal)}"
-            twoColumnView && normalizedValue < QUARTILE * 3 -> "more $stat \n${"%.2f".format(originalVal)}"
-            else -> "super $stat \n${"%.2f".format(originalVal)}"
+            normalizedValue < QUARTILE -> "not at all $stat"
+             normalizedValue < QUARTILE * 2 -> "less $stat"
+            normalizedValue < QUARTILE * 3 -> "more $stat"
+            else -> "super $stat"
         }
     }
 
-    private fun loadAudioFeatures(danceability: StatLabel, energy: StatLabel, valence: StatLabel) {
-        title_row_one.text = danceability.title
-        range_one.start = danceability.range.first
-        range_one.end = danceability.range.second
+    private fun loadAudioFeatures(one: StatLabel, two: StatLabel, three: StatLabel) {
+        title_row_one.text = one.title
+        title_row_one_number.text = one.titleNumber
+        range_one.start = one.range.first
+        range_one.end = one.range.second
 
-        title_row_two.text = energy.title
-        range_two.start = energy.range.first
-        range_two.end = energy.range.second
+        title_row_two.text = two.title
+        title_row_two_number.text = two.titleNumber
+        range_two.start = two.range.first
+        range_two.end = two.range.second
 
-        title_row_three.text = valence.title
-        range_three.start = valence.range.first
-        range_three.end = valence.range.second
+        title_row_three.text = three.title
+        title_row_three_number.text = three.titleNumber
+        range_three.start = three.range.first
+        range_three.end = three.range.second
     }
 
 }
